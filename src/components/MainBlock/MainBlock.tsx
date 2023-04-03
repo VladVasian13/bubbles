9a98099a1359dfa0c16ef3cdd1e64c3387c86415
Vlad Vasian <vlad.vasian96@gmail.com>
2023-04-03 17:54:43 +0300

    Clean-up + creating main block

---
+++ src/components/MainBlock/MainBlock.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./MainBlock.style.css"
+
+const MainBlock = () => {
+    return (
+        <div className="main-container">TEST</div>
+    )
+}
+
+export default MainBlock;
